6e193502e6f6fb1033c3950ca4da1576a9aa056a
Bilanuk <bilanuk.bill@gmail.com>
2023-07-26 17:58:34 +0300

    add note create/update/delete/[un]archive

---
+++ index.js
@@ -0,0 +1,102 @@
+function createNoteObject(content, category) {
+  const timestamp = new Date().toLocaleString();
+  const archived = false;
+  return { timestamp, content, category, archived };
+}
+
+let activeNotes = [];
+let archivedNotes = [];
+
+const noteInput = document.querySelector('#noteInput');
+const categorySelect = document.querySelector('#categorySelect');
+const activeNotesList = document.querySelector('#activeNotesList');
+const archivedNotesList = document.querySelector('#archivedNotesList');
+
+function renderNotes(notes, targetElement) {
+  targetElement.innerHTML = '';
+
+  notes.forEach((note, index) => {
+    const noteDiv = document.createElement('div');
+    noteDiv.innerHTML = `
+      <p><strong>${note.timestamp}</strong></p>
+      <p><em>${note.category}</em></p>
+      <p>${note.content}</p>
+      ${targetElement === activeNotesList
+        ? `<button class="editButton" data-index="${index}">Edit</button>
+           <button class="archiveButton" data-index="${index}">Archive</button>`
+        : `<button class="unarchiveButton" data-index="${index}">Unarchive</button>`}
+      <button class="deleteButton" data-index="${index}">Delete</button>
+    `;
+    targetElement.appendChild(noteDiv);
+  });
+
+  const editButtons = targetElement.querySelectorAll('.editButton');
+  editButtons.forEach(button => button.addEventListener('click', handleEditNote));
+
+  const archiveButtons = targetElement.querySelectorAll('.archiveButton');
+  archiveButtons.forEach(button => button.addEventListener('click', handleArchiveNote));
+
+  const unarchiveButtons = targetElement.querySelectorAll('.unarchiveButton');
+  unarchiveButtons.forEach(button => button.addEventListener('click', handleUnarchiveNote));
+
+  const deleteButtons = targetElement.querySelectorAll('.deleteButton');
+  deleteButtons.forEach(button => button.addEventListener('click', handleDeleteNote));
+}
+
+function rerenderApp() {
+  renderNotes(activeNotes, activeNotesList);
+  renderNotes(archivedNotes, archivedNotesList);
+}
+
+function handleAddNote() {
+  const content = noteInput.value.trim();
+  const category = categorySelect.value;
+
+  if (content === '') return;
+
+  const newNote = createNoteObject(content, category);
+  activeNotes.push(newNote);
+
+  rerenderApp();
+  noteInput.value = '';
+}
+
+function handleEditNote(event) {
+  const index = event.target.dataset.index;
+  const newContent = prompt('Edit the note:', activeNotes[index].content);
+  if (newContent !== null && newContent.trim() !== '') {
+    activeNotes[index].content = newContent.trim();
+    rerenderApp();
+  }
+}
+
+function handleArchiveNote(event) {
+  const index = event.target.dataset.index;
+  const archivedNote = activeNotes.splice(index, 1)[0];
+  archivedNote['archived'] = true;
+  archivedNotes.push(archivedNote);
+  rerenderApp();
+}
+
+function handleUnarchiveNote(event) {
+  const index = event.target.dataset.index;
+  const unarchivedNote = archivedNotes.splice(index, 1)[0];
+  unarchivedNote['archived'] = false;
+  activeNotes.push(unarchivedNote);
+  rerenderApp();
+}
+
+function handleDeleteNote(event) {
+  const index = event.target.dataset.index;
+  if (event.target.parentElement.parentElement === activeNotesList) {
+    activeNotes.splice(index, 1);
+  } else {
+    archivedNotes.splice(index, 1);
+  }
+
+  rerenderApp();
+}
+
+document.querySelector('#addButton').addEventListener('click', handleAddNote);
+
+rerenderApp();
